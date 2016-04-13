766b23beac9dc346a68c2b9c9628c5c7621b1203
Librius <bodu@usc.edu>
2016-04-12 23:01:51 -0700

    city scene update

---
+++ Assets/Scripts/CityPlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CityPlayerController : MonoBehaviour {
+
+	public Sprite littleWhiteRabbit;
+	public GameObject finalGoal;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("SteinMamba")) {
+			other.gameObject.SetActive (false);
+			transform.GetChild (0).gameObject.SetActive (false);
+			transform.GetChild (1).gameObject.SetActive (true);
+
+			GetComponent<PlayerController> ().hasKey = true;
+			GetComponent<PlayerController> ().targetImage.sprite = littleWhiteRabbit;
+
+			GameObject arrow = transform.FindChild ("arrow").gameObject;
+			arrow.GetComponents<ArrowController> () [0].target = finalGoal;
+		}
+		if (other.gameObject.CompareTag ("LittleWhiteRabbit"))
+		{
+			Debug.Log ("Win");
+		}
+
+	}
+}
