8e5c4c66c0bcdaed0ab103f3d8a9705277120ed5
Librius <bodu@usc.edu>
2016-04-13 00:39:30 -0700

    enemy square 2

--- Assets/Scripts/EnemyControllerSquare2.cs
+++ Assets/Scripts/EnemyControllerSquare2.cs
@@ -13,29 +13,29 @@ public class EnemyControllerSquare2 : MonoBehaviour {
 	void Update()
 	{
 		transform.Translate (Vector3.forward * speed * Time.deltaTime);
-//		if (flag && transform.localPosition.z < -280 ) {
-//			double s = Time.deltaTime * 0.07;
-//			float shift = (float)s;
-//			transform.Translate (new Vector3(shift,0,0));
-//		}
+		//		if (flag && transform.localPosition.z < -280 ) {
+		//			double s = Time.deltaTime * 0.07;
+		//			float shift = (float)s;
+		//			transform.Translate (new Vector3(shift,0,0));
+		//		}
 		//		Debug.Log(transform.localPosition.z);
 		if ( transform.localPosition.x > -356 && transform.localPosition.z < -205 && !flag) {
-//			Debug.Log ("2");
+			//			Debug.Log ("2");
 			transform.Rotate (0, -90, 0);
 			flag = !flag;
 		}
 		if ( transform.localPosition.x > -356 && transform.localPosition.z > -139 && flag) {
-//			Debug.Log ("3");
+			//			Debug.Log ("3");
 			transform.Rotate (0, -90, 0);
 			flag = !flag;
 		}
-		if ( transform.localPosition.z > -139 && transform.localPosition.x < -541 && !flag) {
-//			Debug.Log ("4");
+		if ( transform.localPosition.z > -145 && transform.localPosition.x < -541 && !flag) {
+			//			Debug.Log ("4");
 			transform.Rotate (0, -90, 0);
 			flag = !flag;
 		}
-		if ( transform.localPosition.x < -540 && transform.localPosition.z > -208 && flag) {
-//			Debug.Log ("1");
+		if ( transform.localPosition.x < -540 && transform.localPosition.z < -208 && flag) {
+			//			Debug.Log ("1");
 			transform.Rotate (0, -90, 0);
 			flag = !flag;
 		}
